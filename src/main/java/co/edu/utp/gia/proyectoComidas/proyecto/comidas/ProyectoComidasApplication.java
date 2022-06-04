fe4e518eae376fe2eb1e4b2ad48996ca16d181fe
juanjo <juanjose.granadatp.edu.co>
2022-06-04 10:38:34 -0500

    Inicio

---
+++ src/main/java/co/edu/utp/gia/proyectoComidas/proyecto/comidas/ProyectoComidasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.gia.proyectoComidas.proyecto.comidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoComidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoComidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/gia/proyectoComidas/proyecto/comidas/ProyectoComidasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.gia.proyectoComidas.proyecto.comidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoComidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
